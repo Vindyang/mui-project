784a7f2ab07d64f9200b2f2457aa22c9dc37fb29
kaizuuman <140473472+kaizuuman@users.noreply.github.com>
2023-09-23 22:56:43 +0700

    pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,18 @@ import { Button, IconButton } from '@mui/material';
 
 function App() {
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Box
         display="flex"
         flexDirection="row"
         justifyContent="center"
         alignItems="center"
-        height={500}
-        width={1000}
         bgcolor="primary.main"
         color="primary.contrastText"
+        maxWidth="100%"
+        width="100%"
+        height="100%"
+        padding="20px"
       >
         {/* Camera and Controller Box */}
         <Box
@@ -23,11 +25,12 @@ function App() {
           justifyContent="center"
           alignItems="center"
           margin="10px"
+          flex="1.5"
         >
           {/* Camera Box */}
           <Box
-            width={400}
-            height={250}
+            width="100%"
+            height={400}
             bgcolor="secondary.main"
             display="flex"
             flexDirection="column"
@@ -39,8 +42,8 @@ function App() {
 
           {/* Controller Box */}
           <Box
-            width={400}
-            height={150}
+            width="100%"
+            height={250}
             bgcolor="secondary.main"
             display="flex"
             flexDirection="column"
@@ -49,18 +52,18 @@ function App() {
             margin="10px"
           >
             {/* Buttons */}
-            <Button variant="contained" color="primary" style={{ marginBottom: '10px', marginRight: '200px', marginTop:'15px' }}>
+            <Button variant="contained" color="primary" style={{ marginBottom: '10px', marginRight: '600px', marginTop:'15px' }}>
               Home
             </Button>
-            <Button variant="contained" color="secondary" style={{ marginBottom: '10px', marginRight: '200px' }}>
+            <Button variant="contained" color="secondary" style={{ marginBottom: '10px', marginRight: '600px' }}>
               Button 2
             </Button>
-            <Button variant="contained" color="primary"style={{ marginRight: '200px'}}>
+            <Button variant="contained" color="primary"style={{ marginRight: '600px'}}>
               Button 3
             </Button>
             <IconButton>
               {/* Camera controller */}
-              <ControlCameraIcon style={{ fontSize: 48, marginLeft: '150px', position: "absolute", marginBottom: '125px', textAlign: "centre" }} />
+              <ControlCameraIcon style={{ fontSize: 100, marginLeft: '150px', position: "absolute", marginBottom: '125px', textAlign: "centre" }} />
             </IconButton>
           </Box>
         </Box>
@@ -72,11 +75,12 @@ function App() {
           justifyContent="center"
           alignItems="center"
           margin="10px"
+          flex="1"
         >
           {/* X-Y Router Box */}
           <Box
-            width={300} 
-            height={250} 
+            width="100%" 
+            height={400} 
             bgcolor="secondary.main"
             display="flex"
             flexDirection="column"
@@ -88,8 +92,8 @@ function App() {
 
           {/* Controller Box */}
           <Box
-            width={300} 
-            height={150} 
+            width="100%" 
+            height={250} 
             bgcolor="secondary.main"
             display="flex"
             flexDirection="column"
@@ -98,18 +102,18 @@ function App() {
             margin="10px"
           >
             {/* Buttons */}
-            <Button variant="contained" color="primary" style={{ marginBottom: '10px', marginRight: '100px', marginTop:'15px' }}>
+            <Button variant="contained" color="primary" style={{ marginBottom: '10px', marginRight: '300px', marginTop:'15px' }}>
               Home
             </Button>
-            <Button variant="contained" color="secondary" style={{ marginBottom: '10px', marginRight: '100px' }}>
+            <Button variant="contained" color="secondary" style={{ marginBottom: '10px', marginRight: '300px' }}>
               Button 2
             </Button>
-            <Button variant="contained" color="primary" style={{ marginRight: '100px'}}>
+            <Button variant="contained" color="primary" style={{ marginRight: '300px'}}>
               Button 3
             </Button>
             <IconButton>
               {/* X-Y Router controller */}
-              <ControlCameraIcon style={{ fontSize: 48, marginLeft: '150px', position: "absolute", marginBottom: '125px', textAlign: "centre" }} />
+              <ControlCameraIcon style={{ fontSize: 100, marginLeft: '150px', position: "absolute", marginBottom: '125px', textAlign: "centre" }} />
             </IconButton>
           </Box>
         </Box>
